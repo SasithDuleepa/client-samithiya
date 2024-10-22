900cc248925034e29e628d40f0627beca0aa9b5c
sasith duleepa <duleepajayathissa@gmail.com>
2024-10-22 22:43:06 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+
+import Login from './pages/login';
+
+import Dashboard1 from './pages/dashboard1';
+
+import CreateRole from './pages/createRole';
+import CreateUser from './pages/createUser';
+import EditProfile from './pages/editProfile';
+
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Login/> */}
+     {/* <Dashboard1/> */}
+     {/* <CreateRole/> */}
+     {/* <CreateUser/> */}
+     {/* <Sidebar/>
+     <EditProfile/> */}
+
+
+     <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Login/>}/> 
+        <Route path="/dashboard" element={<Dashboard1/>}/> 
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/dashboard1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Dashboard1() {
+  return (
+    <div>Dashboard_1</div>
+  )
+}
